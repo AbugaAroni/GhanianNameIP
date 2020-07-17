888346c914dc76a9d9630eb65bf45f601f835510
AbugaAroni <abugaaroni@gmail.com>
2020-07-17 20:53:14 +0300

    add a working control flow!

--- js/scripts.js
+++ js/scripts.js
@@ -18,46 +18,45 @@
 //number of day of week, sun =1, sat =7
   var nodow;
 
-//function to get day of the week
- function dow(cc,yy,mm,dd)
- {
-   //tofixed returns a single digit
-   return nodow = ((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26*(mm + 1) / 10)) + dd) % 7).toFixed(0);}
+  //function to get day of the week
+   function dow(cc,yy,mm,dd)
+   {
+     return (((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26*(mm + 1) / 10)) + dd) % 7).toFixed(0));}
 
-if(gender === "Male")
-//if statement for selection of male names
-  {if(nodow===1)
-    {
-      yourname= mnames[1];
-    }
-  else if(nodow===2)
-    {
-      yourname= mnnames[2];
-    }
-    else if(nodow===3)
-      {
-        yourname= mnnames[3];
-      }
-      else if(nodow===4)
-        {
-          yourname= mnnames[4];
-        }
-        else if(nodow===5)
-          {
-            yourname= mnnames[5];
-          }
-          else if(nodow===6)
-            {
-              yourname= mnnames[6];
-            }
-            else
-            {
-              yourname= mnames[7];
-            }
-}
-//selection of female names
-else if {
+  nodow = parseInt(dow (cc,yy,mm,dd));
+  console.log(dow (cc,yy,mm,dd));
+  console.log(nodow);
+  console.log(mnames[nodow]+"control");
 
-}
-// should display error
-else
+  //if statement for selection of male names
+
+  if(nodow===0)
+   {yourname = mnames[0];
+  	 console.log(yourname+"sunday");}
+
+      else if (nodow===1)
+   {yourname = mnames[1];
+  	 console.log(yourname+"monday");}
+
+     else if (nodow===2)
+   {yourname = mnames[2];
+  	 console.log(yourname+"tuesday");}
+
+      else if (nodow==3)
+   {yourname = mnames[3];
+  	 console.log(yourname+"wednesday");}
+
+     else if (nodow==4)
+   {yourname = mnames[4];
+  	 console.log(yourname+"AB4UGA");}
+
+     else if (nodow==5)
+   {yourname = mnames[5];
+  	 console.log(yourname+"ABUG6A");}
+
+      else if (nodow==6)
+   {yourname = mnames[6];
+  	 console.log(yourname+"ABA");}
+
+
+   else {console.log("Itdidntwork");}
